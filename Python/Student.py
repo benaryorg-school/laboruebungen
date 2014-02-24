c4ac692230c8f393dfbd0aa915e0fa95b6f13a09
benaryorg <benedikt.bre3@gmail.com>
2014-02-24 09:54:49 +0100

    Fully working "main"

--- Python/Student.py
+++ Python/Student.py
@@ -1,8 +1,15 @@
 from Person import Person
 
+def search(lst,name):
+	lst2=[]
+	for s in lst:
+		if name in s.name:
+			lst2.append(s)
+	return lst2
+
 class Student(Person):
 	def __init__(self,nn,vn,mn):
-		Person.__init__(self,vn,nn)
+		Person.__init__(self,nn,vn)
 		self.setMatrikelnummer(mn)
 
 	def __str__(self):
@@ -14,3 +21,12 @@ class Student(Person):
 if __name__=='__main__':
 	s=Student('Huber','Karl',123456)
 	print s
+	s=[]
+	vn=["Alfred", "Bernd", "Carola", "Dieter", "Erich",	"Gerda", "Hans", "Jochen", "Karin", "Maike"]
+	nn=["Adler", "Baron", "Claus", "Dimov", "Eliot", "Gatti", "Heine","Jahn", "Kozak", "Miller"]
+	mn=[12345, 45671, 23456, 11111, 45667, 98712, 23456, 65123, 12634, 22222]
+	for i in xrange(len(vn)):
+		s.append(Student(nn[i],vn[i],mn[i]))
+	lst=search(s,'er')
+	for s in lst:
+		print s
