b507e8b87528c372ee2c7e370c00ffba436314e3
benaryorg <brenner@benary.org>
2014-04-28 08:51:15 +0200

    Made Liste!

---
+++ Java/src/org/benary/Labs/Liste/Liste.java
@@ -0,0 +1,148 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 benaryorg.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.benary.Labs.Liste;
+
+/**
+ *
+ * @author benaryorg
+ */
+public class Liste
+{
+
+	private Knoten anfang;
+	private Knoten ende;
+	private int laenge;
+
+	public Liste()
+	{
+
+	}
+
+	public Liste(Object data)
+	{
+		this.anfang=this.ende=new Knoten(data);
+		this.laenge++;
+	}
+
+	public Knoten getAnfang()
+	{
+		return anfang;
+	}
+
+	public Knoten getEnde()
+	{
+		return ende;
+	}
+
+	public int getLaenge()
+	{
+		return laenge;
+	}
+
+	public void fuegeAmAnfangHinzu(Object data)
+	{
+		Knoten tmp=new Knoten(data);
+		tmp.next=this.anfang;
+		this.anfang=tmp;
+		if(this.laenge<=0)
+		{
+			this.ende=tmp;
+		}
+		this.laenge++;
+	}
+
+	public void fuegeAmEndeHinzu(Object data)
+	{
+		if(this.laenge<=0)
+		{
+			this.fuegeAmAnfangHinzu(data);
+		}
+		else
+		{
+			Knoten tmp=new Knoten(data);
+			this.ende.next=tmp;
+			this.ende=tmp;
+			this.laenge++;
+		}
+	}
+
+	public void fuegeMittenDrinnenHinzu(int index,Object data) throws ListeOutOfBoundsException
+	{
+		if(index<0||index>this.laenge)
+		{
+			throw new ListeOutOfBoundsException("Index \""+index+"\" is out of bounds");
+		}
+		if(index==0)
+		{
+			this.fuegeAmAnfangHinzu(data);
+		}
+		else
+		{
+			if(index==this.laenge)
+			{
+				this.fuegeAmEndeHinzu(data);
+			}
+			else
+			{
+				Knoten tmp=this.anfang;
+				while(index-->1)
+				{
+					tmp=tmp.next;
+				}
+				Knoten neu=new Knoten(data);
+				neu.next=tmp.next;
+				tmp.next=neu;
+				this.laenge++;
+			}
+		}
+	}
+
+	public Knoten getKnotenAnStelle(int index)
+	{
+		Knoten tmp=this.anfang;
+		while(index-->0)
+		{
+			tmp=tmp.next;
+		}
+		return tmp;
+	}
+
+	public Object getDataAnStelle(int index)
+	{
+		return this.getKnotenAnStelle(index).data;
+	}
+
+	public static void main(String... args)
+	{
+		Liste lst=new Liste();
+		lst.fuegeAmAnfangHinzu(1);
+		lst.fuegeAmAnfangHinzu(0);
+		lst.fuegeAmEndeHinzu(3);
+		lst.fuegeMittenDrinnenHinzu(2,2);
+		for(int i=0;i<lst.getLaenge();i++)
+		{
+			System.out.println(lst.getDataAnStelle(i));
+		}
+	}
+}
